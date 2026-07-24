5f37c6f076a580fe629c141beaf47c6e429e0c3c
Renato Dehnhardt <renatotkd@gmail.com>
2026-07-24 14:44:18 +0200

    Add Welcome page component with basic structure

---
+++ files/resources/js/pages/welcome.tsx
@@ -0,0 +1,13 @@
+import {Head} from '@inertiajs/react';
+
+export default function Welcome() {
+  return (
+    <>
+      <Head title="Welcome"/>
+
+      <div className="min-h-screen flex items-center justify-center">
+        <h1 className="text-4xl font-bold">Welcome</h1>
+      </div>
+    </>
+  );
+}
