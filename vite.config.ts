d65726e9181cc9a305e434cd23fe0498b9e1d838
Renato Dehnhardt <renatotkd@gmail.com>
2026-07-26 18:21:46 +0200

    Add `vite.config.ts` with plugins for Laravel, React, Tailwind, and Inertia setup

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import inertia from '@inertiajs/vite';
+import { wayfinder } from '@laravel/vite-plugin-wayfinder';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import laravel from 'laravel-vite-plugin';
+import { bunny } from 'laravel-vite-plugin/fonts';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.tsx'],
+            refresh: true,
+            fonts: [bunny('Nunito', { weights: [200, 400, 500, 600, 700, 800, 900] })],
+        }),
+        inertia(),
+        react({
+            babel: {
+                plugins: ['babel-plugin-react-compiler'],
+            },
+        }),
+        tailwindcss(),
+        wayfinder({
+            formVariants: true,
+        }),
+    ],
+});
