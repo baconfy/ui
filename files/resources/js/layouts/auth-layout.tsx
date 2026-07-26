d5fd6b1979c9a79a0721f74c0db4d9704bff031d
Renato Dehnhardt <renatotkd@gmail.com>
2026-07-26 20:14:19 +0200

    Enhance `AuthLayout` with `Brand` component and improve UI structure:
    - Add `Brand` component for consistent branding in authentication layouts.
    - Center-align content with updated styles for improved visual hierarchy.

--- files/resources/js/layouts/auth-layout.tsx
+++ files/resources/js/layouts/auth-layout.tsx
@@ -1,5 +1,6 @@
 import { Head } from '@inertiajs/react';
 import type { ReactNode } from 'react';
+import { Brand } from '@/components/ui/brand';
 
 export default function AuthLayout({ title = '', description = '', children }: { title?: string; description?: string; children: ReactNode }) {
     return (
@@ -8,9 +9,13 @@ export default function AuthLayout({ title = '', description = '', children }: {
 
             <div className="mx-auto flex min-h-svh max-w-sm items-center justify-center">
                 <div className="flex w-full flex-col gap-12 p-6 md:p-0">
-                    <div className="flex flex-col gap-4">
-                        <h1 className="text-3xl font-bold leading-8">{title}</h1>
-                        <p className="text-muted-foreground leading-6">{description}</p>
+                    <div className="flex flex-col gap-4 text-center items-center justify-center">
+                        <Brand className="size-24" />
+
+                        <div>
+                            <h1 className="text-3xl leading-8 font-bold">{title}</h1>
+                            <p className="leading-6 text-muted-foreground">{description}</p>
+                        </div>
                     </div>
 
                     <div className="flex flex-col gap-8">{children}</div>
