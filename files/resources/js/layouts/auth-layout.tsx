77a1f3e7a15dd4748cc317f774128c73117ff860
Renato Dehnhardt <renatotkd@gmail.com>
2026-07-26 21:10:52 +0200

    Update `AuthLayout` for improved structure and branding:
    - Refactor layout to use a two-column grid for better content organization.
    - Add background image (`auth.webp`) on larger screens for enhanced visual appeal.
    - Replace `auth.png` with optimized `auth.webp`.

--- files/resources/js/layouts/auth-layout.tsx
+++ files/resources/js/layouts/auth-layout.tsx
@@ -7,18 +7,24 @@ export default function AuthLayout({ title = '', description = '', children }: {
         <>
             <Head title={title} />
 
-            <div className="mx-auto flex min-h-svh max-w-sm items-center justify-center">
-                <div className="flex w-full flex-col gap-12 p-6 md:p-0">
-                    <div className="flex flex-col gap-4 text-center items-center justify-center">
-                        <Brand className="size-24" />
+            <div className="grid min-h-svh lg:grid-cols-2">
+                <div className="flex items-center justify-center p-6 md:p-10">
+                    <div className="flex w-full max-w-sm flex-col gap-12">
+                        <div className="flex flex-col items-center justify-center gap-4 text-center">
+                            <Brand className="size-24" />
 
-                        <div>
-                            <h1 className="text-3xl leading-8 font-bold">{title}</h1>
-                            <p className="leading-6 text-muted-foreground">{description}</p>
+                            <div>
+                                <h1 className="text-3xl leading-8 font-bold">{title}</h1>
+                                <p className="leading-6 text-muted-foreground">{description}</p>
+                            </div>
                         </div>
-                    </div>
 
-                    <div className="flex flex-col gap-8">{children}</div>
+                        <div className="flex flex-col gap-8">{children}</div>
+                    </div>
+                </div>
+
+                <div className="hidden p-3 lg:block">
+                    <img src="/auth.webp" alt="" className="size-full rounded-2xl bg-muted object-cover" />
                 </div>
             </div>
         </>
